d6a370967b49aae9f01c854a035f32dc17527f3a
zmngabriela <zmngabriela@gmail.com>
2025-02-12 18:16:07 +0100

    feat: adding portuguese translation on i18n and for each language name feat: adding transtration for missing skills refactor: removing translation of deleted projects

--- src/util/i18n.js
+++ src/util/i18n.js
@@ -2,11 +2,13 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './locales/en.json';
 import es from './locales/es.json';
+import pt from './locales/pt.json';
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
-    es: { translation: es }
+    es: { translation: es },
+    pt: { translation: pt }
   },
   lng: 'en',
   fallbackLng: 'en',
